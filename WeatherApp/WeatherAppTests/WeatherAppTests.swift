55860218615536240c48dda345310df2f3f94316
pravith <pravi036@gmail.com>
2019-03-04 18:34:47 +0530

    UI Unit Test and UI Changes
    Added Unit Test cases for UI and minor UI xib changes for Lable

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -15,20 +15,41 @@ class WeatherAppTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    //button should have title "Submit"
+    func test_button_title_is_Submit() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let login = storyboard.instantiateInitialViewController() as! ViewController
+        let _ = login.view
+        XCTAssertEqual("Submit",login.fetchWeatherDataButton!.titleLabel?.text!)
     }
-
+    
+    //Textfield should have placeholder "Latitude"
+    func test_latitude_placeholder_is_Latitude() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let login = storyboard.instantiateInitialViewController() as! ViewController
+        let _ = login.view
+        XCTAssertEqual("Latitude", login.latitudeTxtField!.placeholder!)
+        
+    }
+    
+    //Textfield should have placeholder "Longitude"
+    func test_logitude_placeholder_is_Longitude() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let login = storyboard.instantiateInitialViewController() as! ViewController
+        let _ = login.view
+        XCTAssertEqual("Longitude", login.logitudeTxtField!.placeholder!)
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
+
