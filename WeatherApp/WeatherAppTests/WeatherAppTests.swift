247ad5b11255dee64d818e031c51fb96f618d1d0
pravith <pravi036@gmail.com>
2019-03-05 12:04:08 +0530

    Fuctional Testing
    Added API functional Testing

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -10,9 +10,17 @@ import XCTest
 @testable import WeatherApp
 
 class WeatherAppTests: XCTestCase {
+    
+    var sessionUnderTest: URLSession!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
+    }
+    override func tearDown() {
+        sessionUnderTest = nil
+        super.tearDown()
     }
 
     //button should have title "Submit"
@@ -39,11 +47,39 @@ class WeatherAppTests: XCTestCase {
         let _ = login.view
         XCTAssertEqual("Longitude", login.logitudeTxtField!.placeholder!)
     }
+    // Asynchronous test: success fast, failure slow
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        // given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            // then
+            
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
+    }
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
